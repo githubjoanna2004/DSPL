2db6108d3243b6d19a4bdbf4b0ba406b572030ab
ARCHA16 <archasugathan2004@gmail.com>
2024-09-26 21:50:46 +0530

    Circular Convolution using concentric method

---
+++ Experiment4a.m
@@ -0,0 +1,18 @@
+%Circular convolution using concentric circle method
+clc;
+close all;
+clear all;
+x=input("Enter first sequence");
+x=x(:,end:-1:1);
+h=input("Enter second sequence");
+y=[1,max(length(x), length(h))];
+for i=1:length(x)
+x=[x(end) x(1:end-1)];
+y(i)=sum(x.*h);
+end
+disp("Convolution product y:");
+disp(y);
+stem(y,'filled');
+xlabel('n');
+ylabel('y(n)');
+title('circular convolution result');
