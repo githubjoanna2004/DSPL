b2d21d2dfb217ecb82eb67e4c2583583b386f595
ARCHA16 <archasugathan2004@gmail.com>
2024-11-06 13:29:24 +0530

    Update Experiment4a.m

--- Experiment4a.m
+++ Experiment4a.m
@@ -5,7 +5,8 @@ clear all;
 x=input("Enter first sequence");
 x=x(:,end:-1:1);
 h=input("Enter second sequence");
-y=[1,max(length(x), length(h))];
+
+y=[1,max(length(x), length(h))];
 for i=1:length(x)
 x=[x(end) x(1:end-1)];
 y(i)=sum(x.*h);
